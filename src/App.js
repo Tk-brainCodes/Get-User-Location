616e7d50e34c274567775b587e0cf94436944ffc
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-10-03 15:30:56 +0100

    modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React, { lazy, Suspense } from 'react'
 import './App.css'
-import MapComp from './Components/Mapcomponent/MapComp'
+import Spinner from 'react-bootstrap/Spinner'
 require('dotenv').config()
+const MapComp = lazy(() => import('./Components/Mapcomponent/MapComp'))
 
 function App () {
   return (
     <div className='App'>
-      <MapComp />
+      <Suspense
+        fallback={
+          <Spinner animation='border' role='status'>
+            <span className='visually-hidden'>Loading...</span>
+          </Spinner>
+        }
+      >
+        <MapComp />
+      </Suspense>
     </div>
   )
 }
--- src/Components/Marker/Marker.js
+++ src/Components/Marker/Marker.js
@@ -1,20 +1,18 @@
 import React from 'react'
-import MapIcon from './icon.svg'
 
 const AnyReactComponent = () => {
   return (
-    <div
-      style={{
-        width: '40px',
-        height: '40px',
-        padding: '10px',
-        color: 'white',
-        display: 'flex',
-        alignItemns: 'center',
-        justifyContent: 'center'
-      }}
-    >
-      <img src={MapIcon} alt='marker location icon' />
+    <div className='w-40 h-40 d-flex'>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='40'
+        height='40'
+        fill='#94ECBE'
+        class='bi bi-geo-alt-fill'
+        viewBox='0 0 16 16'
+      >
+        <path d='M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z' />
+      </svg>
     </div>
   )
 }
